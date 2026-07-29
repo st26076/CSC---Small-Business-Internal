980b80451d236d7747e98dac832586452ad89712
Hamsika <st26076@ormiston.school.nz>
2026-07-29 12:29:16 +1200

    Overall - Finished the about page including the paragraph and the visual, eye-catching image. Fixed the navigation bar since it did not work originally. Added more CSS files, and changed the image.

--- app.py
+++ app.py
@@ -8,32 +8,24 @@ app.secret_key = 'cookies_are_an_emotion'
 
 @app.route('/')
 def index():
-
+    
     return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-
     return render_template('about.html')
 
 
 @app.route('/order_history')
 def order_history():
-
     return render_template('order_history.html')
 
 
 @app.route('/invoice')
 def invoice():
-
     return render_template('/invoice.html')
 
 
-
-
-
-
 if __name__ == '__main__':
-    initialise_database()
     app.run(debug=True)
