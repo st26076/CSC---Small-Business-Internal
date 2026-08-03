1d600e93c99eba8111fe18476e9e0e081651fd18
Hamsika <st26076@ormiston.school.nz>
2026-08-03 16:21:08 +1200

    Added the initialise_database code, which updates the orders to SQLite to make it easier to read the orders for the students running the business. I also added the cookies, frosting, and toppings variables in the '/' route. The load data has also been linked to the respective variables so that the details can be collected.

--- app.py
+++ app.py
@@ -6,12 +6,41 @@ from flask import Flask, render_template, request, session, flash, redirect, url
 app = Flask(__name__)
 app.secret_key = 'cookies_are_an_emotion'
 
+def initialise_database():
+    with sqlite3.connect('unique_cookie.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+                        CREATE TABLE IF NOT EXISTS orders (
+                        order_id INTERGER PRIMARY KEY AUTOINCREMENT,
+                        invoice_number TEXT,
+                        customer_name TEXT,
+                        items TEXT,
+                        frosting TEXT,
+                        toppings TEXT,
+                        total REAL,
+                        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        )
+                        ''')
+
 @app.route('/')
 def index():
+    cookies, frosting, toppings = load_data()
+    return render_template('index.html', cookies=cookies, frosting=frosting, toppings=toppings)
+
+def load_data():
+    try:
+        with open('data/cookie.json') as file:
+            cookies = json.load(file)
+        with open('data/frosting.json') as file:
+            frosting = json.load(file)
+        with open('data/topping.json') as file:
+            toppings = json.load(file)
+        return cookies, frosting, toppings
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading data: {e}")
+    return {}, {}, {}
+
     
-    return render_template('index.html')
-
-
 @app.route('/about')
 def about():
     return render_template('about.html')
