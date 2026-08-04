411e5d035a61913287c50adddb8aba7f3042f821
Hamsika <st26076@ormiston.school.nz>
2026-08-04 16:16:34 +1200

    Linked the JSON files to load_data so that the details can be aquired from the JSON files. Made sure that the chosen cookie, frosting, and toppings are saved as variables in app.py.

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def initialise_database():
 
 @app.route('/')
 def index():
-    cookies, frosting, toppings = load_data()
-    return render_template('index.html', cookies=cookies, frosting=frosting, toppings=toppings)
+    cookies, frostings, toppings = load_data()
+    return render_template('index.html', cookies=cookies, frostings=frostings, toppings=toppings)
 
 def load_data():
     try:
@@ -33,12 +33,88 @@ def load_data():
             cookies = json.load(file)
         with open('data/frosting.json') as file:
             frosting = json.load(file)
-        with open('data/topping.json') as file:
+        with open('data/toppings.json') as file:
             toppings = json.load(file)
         return cookies, frosting, toppings
     except (FileNotFoundError, json.JSONDecodeError) as e:
         print(f"Error loading data: {e}")
     return {}, {}, {}
+
+@app.route('/cookie_to_cart', methods=['POST'])
+def cookie_to_cart():
+    cookie = request.form['cookie']
+    cookies, _, _ = load_data
+    cart = session.get('cart', {})
+
+    if cookie not in cookies:
+        flash("Invalid cookie selected")
+        return redirect(url_for('index'))
+
+    if cookie in cart:
+        cart[cookie]
+
+    else:
+        cart[cookie] = {
+            'price': cookies[cookie]['price']
+        }
+
+    session['cart'] = cart
+    session.modified = True
+    flash(f"{cookie} added to cart")
+    return redirect(url_for('index'))
+
+
+@app.route('/frosting_to_cart', methods=['POST'])
+def frosting_to_cart():
+    frosting = request.form['frosting']
+    _, frostings, _ = load_data
+    cart = session.get('cart', {})
+
+    if frosting not in frostings:
+        flash("Invalid frosting selected")
+        return redirect(url_for('index'))
+
+    if frosting in cart:
+        cart[frosting]
+
+    else:
+        cart[frosting] = {
+            'price': frostings[frosting]['price']
+        }
+
+    session['cart'] = cart
+    session.modified = True
+    flash(f"{frosting} added to cart")
+    return redirect(url_for('index'))
+
+@app.route('/toppings_to_cart', methods=['POST'])
+def toppings_to_cart():
+    toppings_to_cart = {}
+    toppings = load_data()
+
+    selected_keys = request.form.getlist('toppings')
+
+    for topping in selected_keys:
+        if topping in toppings:
+            toppings_to_cart[topping] = {
+                'price': toppings[topping]['price']
+            }
+
+    if not selected_keys:
+            flash("Invalid toppings selected")
+            return redirect(url_for('index'))
+
+    if topping in toppings_to_cart:
+            toppings_to_cart[topping]
+
+    else: toppings_to_cart[topping] = {
+            'price': toppings[topping]['price']
+        }
+
+    session['toppings_to_cart'] = toppings_to_cart
+    session.modified = True
+    flash(f"{topping}(s) added to cart")
+    return redirect(url_for('index'))
 
     
 @app.route('/about')
